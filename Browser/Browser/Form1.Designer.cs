4eb0e9017be1e42794c15c565f3c24518029aebf
Tsegab432 <116906490+Tsegab432@users.noreply.github.com>
2024-01-20 15:09:24 -0500

    Add files via upload

---
+++ Browser/Browser/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(291, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "00:00:00";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(504, 123);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(57, 26);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(579, 123);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(57, 26);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(655, 123);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(57, 26);
+            this.numericUpDown3.TabIndex = 6;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 445);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Time Limiter";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Browser/Browser/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Browser
+{
+    public partial class Form1 : Form
+    {
+        public int hour;
+        public int minute;
+        public int second;
+        private bool isTimerFinished = false;
+
+        private string logFilePath = "usage_log.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // Check if the user has opened the computer today
+            if (HasUserOpenedToday())
+            {
+                ShowAutoCloseMessageBox("You have already opened the computer today.", "Notification", 2000);
+                await Task.Delay(3000);
+                Process.Start("shutdown", "/s /t 0");
+            }
+
+            // Get the values from numericUpDown controls
+            hour = (int)numericUpDown1.Value;
+            minute = (int)numericUpDown2.Value;
+            second = (int)numericUpDown3.Value;
+
+            // Validate values to avoid negative or invalid inputs
+            if (hour < 0 || minute < 0 || second < 0)
+            {
+                ShowAutoCloseMessageBox("Please enter valid values for hours, minutes, and seconds.", "Notification", 2000);
+                return;
+            }
+
+            isTimerFinished = false; // Reset the flag
+
+            for (int hours = 0; hours <= hour; hours++)
+            {
+                for (int minutes = 0; minutes <= minute; minutes++)
+                {
+                    for (int seconds = 0; seconds <= second; seconds++)
+                    {
+                        label1.Text = $"{hours.ToString("D2")}:{minutes.ToString("D2")}:{seconds.ToString("D2")}";
+                        await Task.Delay(1000);
+
+                        // Check if the timer has finished
+                        if (hours == hour && minutes == minute && seconds == second)
+                        {
+                            isTimerFinished = true;
+                            ShowAutoCloseMessageBox("Timer has finished!", "Notification", 2000);
+
+                            // Countdown from 5 seconds and then shut down
+                            for (int s = 5; s >= 0; s--)
+                            {
+                                ShowAutoCloseMessageBox($"Shutting down computer in {s} seconds.", "Countdown", 1000);
+                                await Task.Delay(1000);
+                            }
+
+                            // Shut down the computer
+                            Process.Start("shutdown", "/s /t 0");
+
+                            // Log the usage for today
+                            LogUserOpening();
+                        }
+
+                        // Notify user when half of the time has passed
+                        if (hours == hour / 2 && minutes == minute / 2 && seconds == second / 2)
+                        {
+                            ShowAutoCloseMessageBox($"You have {hour / 2} hours, {minute / 2} minutes, and {second / 2} seconds left. Save all your work!", "Notification", 5000);
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool HasUserOpenedToday()
+        {
+            // Check if the log file exists
+            if (System.IO.File.Exists(logFilePath))
+            {
+                // Read the last usage date from the file
+                string lastUsageDate = System.IO.File.ReadAllText(logFilePath);
+
+                // Compare the last usage date with today's date
+                return lastUsageDate == DateTime.Now.ToString("yyyy-MM-dd");
+            }
+
+            return false; // Log file doesn't exist, meaning the user has not opened the computer today
+        }
+
+        private void LogUserOpening()
+        {
+            // Write today's date to the log file
+            System.IO.File.WriteAllText(logFilePath, DateTime.Now.ToString("yyyy-MM-dd"));
+        }
+
+        private void ShowAutoCloseMessageBox(string message, string caption, int duration)
+        {
+            var autoCloseForm = new AutoCloseForm(message, caption, duration);
+            autoCloseForm.ShowDialog();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class AutoCloseForm : Form
+    {
+        private Timer timer = new Timer();
+
+        public AutoCloseForm(string message, string caption, int duration)
+        {
+            InitializeComponent();
+
+            label1.Text = message;
+            Text = caption;
+
+            timer.Interval = duration;
+            timer.Tick += (sender, args) =>
+            {
+                timer.Stop();
+                this.Close();
+            };
+
+            timer.Start();
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 100);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Message";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AutoCloseForm
+            // 
+            this.ClientSize = new System.Drawing.Size(300, 100);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AutoCloseForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Label label1;
+    }
+}
